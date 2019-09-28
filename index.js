dd9b9ede30c896ba89fdb34605497f6155011696
Kevin Wong <kevin@kevinwong.co>
2019-09-28 19:15:35 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const Keyv = require('keyv')
 const discord_email = new Keyv(CONFIG.DATABASE_URL, { namespace: 'discord_email' })
 const code_email_temp = new Keyv(CONFIG.DATABASE_URL, { namespace: 'code_email_temp' })
 const code_discord_temp = new Keyv(CONFIG.DATABASE_URL, { namespace: 'code_discord_temp' })
-const ALPHANUM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const ALPHANUM = '0123456789'
 
 code_discord_temp.clear()
 code_email_temp.clear()
